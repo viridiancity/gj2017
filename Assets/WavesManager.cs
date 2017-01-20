1bd41369dc8c5a6547169bc1552e209ee519235b
Arvin Gunawan <Arvin Gunawan>
2017-01-21 00:50:29 +1100

    Placeholder wave

---
+++ Assets/WavesManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavesManager : MonoBehaviour
+{
+   public GameObject wavePrefab;
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetButtonDown("Fire1"))
+      {
+         Debug.Log("Hello!?");
+         Instantiate(wavePrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.rotation);
+      }
+   }
+
+   public void DestroyWave()
+   {
+      Debug.Log("Wave Destroying");
+      Destroy(gameObject);
+   }
+}
