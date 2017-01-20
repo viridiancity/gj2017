d20d0669b9a6bcc319063771a61b314dbc9936df
Arvin Gunawan <Arvin Gunawan>
2017-01-21 00:04:58 +1100

    BoatMovement left/right working. Placeholder barrell

--- Assets/BoatMovement.cs
+++ Assets/BoatMovement.cs
@@ -3,17 +3,40 @@ using System.Collections;
 
 public class BoatMovement : MonoBehaviour
 {
-   public float speed; 
+   public Transform endPosition;
+   public float speed;
+   public int movingDirection = 1;
+
+   private Vector3 startPosition;
 
    // Use this for initialization
    void Start()
    {
-
+      startPosition = transform.position;
    }
 
    // Update is called once per frame
    void Update()
    {
       transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+      if (movingDirection == 1)
+      {
+         if (transform.position.x >= endPosition.position.x)
+         {
+            transform.Rotate(Vector3.up * 180f);
+            movingDirection = 0;
+         }
+            
+      }
+      else if (movingDirection == 0)
+      {
+         if (transform.position.x <= startPosition.x)
+         {
+            transform.Rotate(Vector3.up * -180f);
+            movingDirection = 1;
+         }
+      }
+      
    }
 }
