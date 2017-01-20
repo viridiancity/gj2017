8e944fe531771f2d50fe2402be915113879cd6db
Arvin Gunawan <Arvin Gunawan>
2017-01-20 23:28:50 +1100

    First commit?

---
+++ Assets/BoatMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMovement : MonoBehaviour
+{
+   public float speed; 
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      transform.Translate(Vector3.right * speed * Time.deltaTime);
+   }
+}
---
+++ Assets/RemoveSpriteRenderer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveSpriteRenderer : MonoBehaviour
+{
+
+   // Use this for initialization
+   void Start()
+   {
+      SpriteRenderer sr = GetComponent<SpriteRenderer>();
+      Destroy(sr);
+   }
+}
