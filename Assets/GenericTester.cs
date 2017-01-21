ec9cc30784eb20cd2257c30de48514e79f0be0c0
Arvin Gunawan <Arvin Gunawan>
2017-01-21 12:26:43 +1100

    Garbage Bag explosion, health system

---
+++ Assets/GenericTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTester : MonoBehaviour
+{
+   Rigidbody2D rb2d;
+
+   // Use this for initialization
+   void Start()
+   {
+      rb2d = GetComponent<Rigidbody2D>();
+      rb2d.AddForce(new Vector2(35f, 100f));
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+   }
+}
---
+++ Assets/Scripts/TrashScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+   public GameObject smallTrashPrefab;
+   public int amountToSpawn;
+   public float explosionForce;
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+      string tag = collision.transform.tag;
+      if (tag == "Garbage Bag" ||
+          tag == "Waste Barrell" ||
+          tag == "Mines" ||
+          tag == "Torpedo" ||
+          tag == "CementLegGuy")
+      {
+         for (int i = 0; i < amountToSpawn; i++)
+         {
+            GameObject smallTrash = Instantiate(smallTrashPrefab, transform.position, Quaternion.identity);
+
+            float xForce = Random.Range(-5f, 5f);
+            float yForce = Random.Range(0f, 5f);
+
+            smallTrash.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(xForce, yForce) * explosionForce);
+         }
+
+         Destroy(gameObject);
+
+      }
+   }
+}
