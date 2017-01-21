c83950d7f237647066fe2eaddfcdb4ea1ef43ed1
Arvin Gunawan <Arvin Gunawan>
2017-01-21 13:05:01 +1100

    Reorganized files. Added enemywavesmanager

---
+++ Assets/Scripts/EnemyLevelWaveManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLevelWaveManager : MonoBehaviour
+{
+   private static EnemyLevelWaveManager _instance;
+   public static EnemyLevelWaveManager instance
+   {
+      get
+      {
+         if (_instance == null)
+            _instance = GameObject.FindObjectOfType<EnemyLevelWaveManager>();
+
+         return _instance;
+      }
+   }
+   public Transform spawnPosition;
+   public GameObject BarrelBoatPrefab;
+   public GameObject GarbageBoatPrefab;
+
+   List<BoatScript> boats;
+   int enemyWaveNum = 1;
+
+   void Start()
+   {
+
+   }
+
+   void Update()
+   {
+
+   }
+
+   void SpawnWave1()
+   {
+
+   }
+
+   IEnumerator DelayedSpawnBoat(GameObject prefab, float delay)
+   {
+      yield return new WaitForSeconds(delay);
+
+   }
+}
