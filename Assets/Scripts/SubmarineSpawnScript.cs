3c90b1282c5c6e1e20f5ed4c043390349a4de7ae
viridiancity <viridiancity@gmail.com>
2017-01-21 13:20:09 +1100

    Updated SubmarineSpawnScript and scene prefabs

--- Assets/Scripts/SubmarineSpawnScript.cs
+++ Assets/Scripts/SubmarineSpawnScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SubmarineSpawnScript : MonoBehaviour {
 
+    public int layer; // layer 1, 2, 3
+
+    public string side;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +17,16 @@ public class SubmarineSpawnScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void Spawn()
+    {
+        if ( side == "Right")
+        {
+
+        } else if ( side == "Left")
+        {
+
+        }
+    }
+
 }
