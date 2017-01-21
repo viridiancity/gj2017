9fe0da60cf05e86bb8e8d61a4c0bf6a6a6174637
viridiancity <viridiancity@gmail.com>
2017-01-21 18:17:51 +1100

    Added Torpedo prefab and script

--- Assets/TorpedoScript.cs
+++ Assets/TorpedoScript.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class TorpedoScript : MonoBehaviour {
 
+    private enum STATE { SPAWN, ASCENDING, PEAK, DESCENDING }
+
+    private STATE state;
+
 	// Use this for initialization
 	void Start () {
-		
+        state = STATE.SPAWN;
+            fire();
+        state = STATE.ASCENDING;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        switch (state)
+        {
+            case STATE.ASCENDING:
+                // fire to peak target
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.5f, 160));
+                break;
+            case STATE.PEAK:
+
+                break;
+            case STATE.DESCENDING:
+
+                break;
+
+        }
+    }
+
+    public void fire()
+    {
+
+    }
+
 }
