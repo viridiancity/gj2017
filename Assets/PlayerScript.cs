4097d4bd3a288bc47d0ea633b40a44a419e922d9
viridiancity <viridiancity@gmail.com>
2017-01-21 12:15:06 +1100

    Added Wipeout special move

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -8,7 +8,7 @@ public class PlayerScript : MonoBehaviour
     
     public Vector3 endScale = new Vector3(1.25f, 1.25f, 1.25f);
 
-    private Vector3 start = new Vector3(1, 1, 1);
+    private Vector3 startScale = new Vector3(1, 1, 1);
     private Vector3 current = new Vector3(0, 0, 0);
     private bool charging = false;
     private float lTime = 0;
@@ -17,14 +17,28 @@ public class PlayerScript : MonoBehaviour
     public float chargeTime = 1f;
     // Update is called once per frame
 
-    private GameObject wipeout;
+    public GameObject wipeoutObj;
+    public Vector3 endWPos   = new Vector3 (1, 6.5f, 1);
+    public Vector3 startWPos = new Vector3(1, -11f, 1);
+    private bool isWipeout;
 
     void Update()
     {
-        if (charging)
+        if (isWipeout)
         {
-            current = Vector3.Lerp(start, endScale, lTime);
-            transform.localScale = Vector3.Lerp(start, endScale, lTime);
+            if ( !wipeoutObj.GetComponent<WipeoutScript>().isWipeout )
+            {
+                WipeOut();
+            } if ( wipeoutObj.GetComponent<WipeoutScript>().ended )
+            {
+                isWipeout = false;
+            }
+
+        } else if (charging)
+        {
+
+            current = Vector3.Lerp(startScale, endScale, lTime);
+            transform.localScale = Vector3.Lerp(startScale, endScale, lTime);
             lTime += Time.deltaTime;
 
         }
@@ -34,7 +48,7 @@ public class PlayerScript : MonoBehaviour
             charging = false;
 
             transform.localScale = new Vector3(1, 1, 1);
-            start = new Vector3(1, 1, 1);
+            startScale = new Vector3(1, 1, 1);
             /*if ( current.x > start.x && current.y > start.y )
             {
                 start = new Vector3(1, 1, 1);
@@ -57,7 +71,9 @@ public class PlayerScript : MonoBehaviour
         if (scharge + lTime >= scharge + chargeTime)
         {
             // fire epic
-            WipeOut();
+            //lTime = 0;
+            isWipeout = true;
+            //WipeOut();
 
         }
 
@@ -65,19 +81,19 @@ public class PlayerScript : MonoBehaviour
         charging = false;
 
         transform.localScale = new Vector3(1, 1, 1);
-        start = new Vector3(1, 1, 1);
+        startScale = new Vector3(1, 1, 1);
 
     }
 
     void OnMouseDown()
     { // shouldn't be able to be called twice with one finger touch inputs without triggering OnMouseUp first .. hopefully
-        if ( !charging)
+        if ( !charging && !isWipeout )
         {
             charging = true;
             lTime = 0;
             scharge = Time.time;
             transform.localScale = new Vector3(1, 1, 1);
-            start = new Vector3(1, 1, 1);
+            startScale = new Vector3(1, 1, 1);
         }
 
     }
@@ -91,6 +107,20 @@ public class PlayerScript : MonoBehaviour
     { // fire in WaveManager, or spawn wipeout wave
         Debug.Log("Wipeout");
 
+        wipeoutObj.GetComponent<WipeoutScript>().startWipeout();
+
+        /*current = Vector3.Lerp(startWPos, endWPos, lTime);
+        wipeoutObj.transform.localPosition = current;// Vector3.Lerp(new Vector3(0,0,0), endWPos, lTime);
+        lTime += Time.deltaTime;
+        if (wipeoutObj.GetComponent<Transform>().localPosition.y > endWPos.y)
+        {
+            // reset wipeout status
+            wipeoutObj.GetComponent<Transform>().localPosition = startWPos;
+            isWipeout = false;
+            charging = false;
+            lTime = 0;
+            Debug.Log("Reset Wipeout");
+        }*/
     }
 
     IEnumerator ScaleObject()
