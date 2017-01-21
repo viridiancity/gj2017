23e5f069bdbf7d3ecc4397e81b548a1e5e247945
viridiancity <viridiancity@gmail.com>
2017-01-21 14:00:03 +1100

    Submarine spawn and behaviour pass

---
+++ Assets/Scripts/SubmarineScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineScript : MonoBehaviour {
+
+    public Vector3 startpos;
+    public Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // move to target, fire, move off screen
+
+
+	}
+
+    public void init(Vector3 startpos, Vector3 target, string dir)
+    {
+        transform.localPosition = startpos;
+
+        if ( dir == "LEFT" )
+        {
+            // multiply rotation as appropriate to flip sprite direction
+        } else
+        {
+            // right
+
+        }
+    }
+
+}
--- Assets/Scripts/SubmarineSpawnScript.cs
+++ Assets/Scripts/SubmarineSpawnScript.cs
@@ -7,6 +7,9 @@ public class SubmarineSpawnScript : MonoBehaviour {
     public int layer; // layer 1, 2, 3
 
     public string side;
+    public GameObject sub;
+    public GameObject SubmarineTarget;
+    //public GameObject shipfact;
 
 	// Use this for initialization
 	void Start () {
@@ -18,15 +21,25 @@ public class SubmarineSpawnScript : MonoBehaviour {
 		
 	}
 
-    void Spawn()
+    void Spawn() // does not necessarily need to differentiate
     {
+        GameObject o = Instantiate(sub);
         if ( side == "Right")
         {
-
+            o.GetComponent<SubmarineScript>().init(transform.localPosition, SubmarineTarget.transform.localPosition, "LEFT");
         } else if ( side == "Left")
         {
-
+            
+            o.GetComponent<SubmarineScript>().init(transform.localPosition, SubmarineTarget.transform.localPosition, "RIGHT");
         }
+
+        //GameObject s = shipfact.GetComponent<ShipFactory>().getSub(SubmarineTarget);
+
+        //GameObject s = Instantiate(sub, transform.localPositionm, Quaternion.identity);
+        //o.initia enemy.Initialize("Darth Vader", 9000, "lightsaber", true);
+
+        //Instantiate( sub )
+
     }
 
 }
