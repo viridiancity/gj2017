ae1049345e2f641b45a3594e68d73d2570353dad
Arvin Gunawan <Arvin Gunawan>
2017-01-22 03:39:53 +1100

    Main menu to start game implemented. Added a glow to Poseidon's health possibly to indicate  availability of wipeout

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+   Animator animator;
+
+   // Use this for initialization
+   void Start()
+   {
+      animator = GetComponent<Animator>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   public void MoveToGame()
+   {
+      animator.Play("CameraMenuToGame");
+   }
+
+}
