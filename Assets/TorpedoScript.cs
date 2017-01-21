3c7fdd7c0612da29a51f1dc8441ce5e05bc31a87
viridiancity <viridiancity@gmail.com>
2017-01-21 18:21:43 +1100

    Added y-level threshold to torpedo script

--- Assets/TorpedoScript.cs
+++ Assets/TorpedoScript.cs
@@ -7,7 +7,7 @@ public class TorpedoScript : MonoBehaviour {
     private enum STATE { SPAWN, ASCENDING, PEAK, DESCENDING }
 
     private STATE state;
-
+    public float ythresh; // height for torpedo to ascend to
 	// Use this for initialization
 	void Start () {
         state = STATE.SPAWN;
@@ -23,6 +23,12 @@ public class TorpedoScript : MonoBehaviour {
             case STATE.ASCENDING:
                 // fire to peak target
                 gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.5f, 160));
+
+                if ( transform.localPosition.y > ythresh)
+                {
+                    state = STATE.PEAK;
+                }
+
                 break;
             case STATE.PEAK:
 
